0787a3ab46462ecb0bde4cfaebb78d12e5bfabab
Allison Rodrigues <allison_rps@hotmail.com>
2026-02-25 18:39:03 -0300

    Ajustes deploy Frontend

--- Autonomax.Frontend/src/pages/DetalhesCliente.tsx
+++ Autonomax.Frontend/src/pages/DetalhesCliente.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { Layout } from '../components/Layout';
 import { 
